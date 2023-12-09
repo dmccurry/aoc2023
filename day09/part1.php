48277fb9174b50ef149df214b1a65deb6ec487b3
dmccurry <dmccurry@cvent.com>
2023-12-09 15:37:12 -0400

    Day 9

---
+++ day09/part1.php
@@ -0,0 +1,27 @@
+<?php
+$file = file_get_contents("./input");
+$file_lines = explode("\n", $file);
+$lines = array();
+foreach ($file_lines as $file_line) {
+    $lines[] = array_map(fn($v) => intval($v), explode(" ", $file_line));
+}
+$total = 0;
+foreach ($lines as $line) {
+    $total += $line[count($line) - 1] + get_history($line);
+}
+print "Solution is $total\n";
+
+function get_history($arr) {
+    $arr_new = array();
+    $all_zeroes = true;
+    for ($i = 0; $i < count($arr) - 1; $i++) {
+        $new_a = $arr[$i+1] - $arr[$i];
+        if ($new_a != 0) $all_zeroes = false;
+        $arr_new[] = $new_a;
+    }
+    if ($all_zeroes) {
+        return 0;
+    } else {
+        return $arr_new[count($arr_new) - 1] + get_history($arr_new);
+    }
+}
